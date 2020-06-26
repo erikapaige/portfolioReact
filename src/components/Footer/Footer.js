13a037031a20cd3128d946619c8e37b90364e66b
Erika <erika.paige.handley@gmail.com>
2020-06-25 21:18:51 -0700

    sticky footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,61 +1,71 @@
 // bring in react
-import React from "react"
+import React, { Component } from 'react'
 // bring in stylesheet
-import './Footer.css'
+// import './Footer.css'
+import {
+  Navbar, 
+  Container,
+  NavbarBrand
+} from 'reactstrap'
 
-function Footer() {
+var style = {
+  backgroundColor: "#1A1406",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%",
+}
+
+var phantom = {
+  display: 'block',
+  padding: '20px',
+  height: '30px',
+  width: '100%',
+}
+
+const Footer = ({ children }) => {
+  console.log(children)
+
   return (
-    <div className="footer">
-      <div className="container">
-        <div className="row">
-          {/* Col 1 */}
-          <div className="col">
-            <h4>E.P.HANDLEY</h4>
-            <ul className="list-unstyled">
-              <li>513-646-8965</li>
-              <li>Orange County, CA</li>
-              <li>Blah Blah Blah</li>
-            </ul>
-          </div>
-          {/* Col 2 */}
-          <div className="col">
-            <h4>STUFF</h4>
-            <ul className="list-unstyled">
-              <li>513-646-8965</li>
-              <li>Orange County, CA</li>
-              <li>Blah Blah Blah</li>
-            </ul>
-          </div>
-          {/* col 3 */}
-          <div className="col">
-            <h4>STUFF</h4>
-            <ul className="list-unstyled">
-              <li>513-646-8965</li>
-              <li>Orange County, CA</li>
-              <li>Blah Blah Blah</li>
-            </ul>
-          </div>
-        </div>
-        <hr />
-          <div>
-            <div className="row">
-              <p className="col-sm">
-                E.P.HANDLEY || Orange County, CA 
-              </p>
-              <p className="col-sm">
-              E.P.HANDLEY || Orange County, CA
-              </p>
-              <p className="col-sm">
-              E.P.HANDLEY || Orange County, CA
-              </p>
-               <p className="col-sm">
-                E.P.HANDLEY || Orange County, CA 
-              </p>
-            </div>
-          </div>
+    <div>
+      <div style={phantom} />
+      <div style={style}>
+        {/* {children} */}
       </div>
     </div>
   )
 }
+
+// function Footer() {
+//   return (
+    // <div className="footer">
+    //   <div className="container">
+    //       <div className="row">
+    //           <p className="col-sm">
+    //             E.P.HANDLEY || Orange County, CA 
+    //           </p>
+    //       </div>
+    //   </div>
+    // </div>
+//   )
+// }
+
+// class Footer extends Component {
+//   render() {
+//     return (
+//       <div className="fixed-bottom">
+//         <Navbar color="dark" dark>
+//           <Container>
+//             <NavbarBrand>Footer</NavbarBrand>
+//           </Container>
+//         </Navbar>
+//       </div>
+//     )
+//   }
+// }
 
 export default Footer
