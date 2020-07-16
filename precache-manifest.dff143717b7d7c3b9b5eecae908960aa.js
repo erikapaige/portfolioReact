e6aadb3787b96c4b12acd22d3e01ed1421c644d4
Erika <erika.paige.handley@gmail.com>
2020-07-15 18:31:08 -0700

    Updates

---
+++ precache-manifest.dff143717b7d7c3b9b5eecae908960aa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f72c3871f1177a11c199346bb91fc89",
+    "url": "/portfolioWithReact/index.html"
+  },
+  {
+    "revision": "c7b39ecb144a91e2945d",
+    "url": "/portfolioWithReact/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a7a1c179936a8b8af933",
+    "url": "/portfolioWithReact/static/css/main.f593b62d.chunk.css"
+  },
+  {
+    "revision": "c7b39ecb144a91e2945d",
+    "url": "/portfolioWithReact/static/js/2.0df7fa34.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolioWithReact/static/js/2.0df7fa34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a1c179936a8b8af933",
+    "url": "/portfolioWithReact/static/js/main.97b3f0e4.chunk.js"
+  },
+  {
+    "revision": "bac20bf0119053b932fc",
+    "url": "/portfolioWithReact/static/js/runtime-main.b3f45022.js"
+  },
+  {
+    "revision": "21b8b2675821693a286e3e4f08395477",
+    "url": "/portfolioWithReact/static/media/aboutMe.21b8b267.jpg"
+  },
+  {
+    "revision": "d8a75d370ea68375b501f2ca93bc3494",
+    "url": "/portfolioWithReact/static/media/aboutMeHi.d8a75d37.jpg"
+  },
+  {
+    "revision": "b3b1d9be3be363094160b894189fc227",
+    "url": "/portfolioWithReact/static/media/budgetTracker.b3b1d9be.png"
+  },
+  {
+    "revision": "9ec4b8088b02d6a0fa30b1bbdccde13a",
+    "url": "/portfolioWithReact/static/media/fitnessTracker.9ec4b808.png"
+  },
+  {
+    "revision": "931bb2327c6e4173221b8cfb5aad6de3",
+    "url": "/portfolioWithReact/static/media/noteTaker.931bb232.png"
+  },
+  {
+    "revision": "b971d1b28e37863e31ff175c8550a461",
+    "url": "/portfolioWithReact/static/media/project2Listivity.b971d1b2.png"
+  },
+  {
+    "revision": "548aed97a29a81fde955028010b230e4",
+    "url": "/portfolioWithReact/static/media/projectOne.548aed97.png"
+  }
+]);
